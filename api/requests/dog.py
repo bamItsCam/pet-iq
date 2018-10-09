0c60ae9e3d4ccdba4ac4ce801948d9fa66fd38dd
john <johncusackccm@gmail.com>
2018-10-08 22:36:44 -0400

    fixed some things

--- api/requests/dog.py
+++ api/requests/dog.py
@@ -1,24 +1,6 @@
 #!/usr/bin/env python3
-from flask_restful import Resource, Api, reqparse
 
-api = Api(app)
-
-class DogRequest():
-    parser = reqparse.RequestParser()
-    parser.add_argument('name',
-                        type=string,
-                        required=True,
-                        help="This field cannot be left blank"
-                        )
-    parser.add_argument('breed',
-                        type=string,
-                        required=True,
-                        help="This field cannot be left blank"
-                        )
-
-    def post(self, name):
-        data = DogRequests.parser.parse_args()
-        dog_data = {"name": data['name'], "breed", data['breed']}
-        return dog_data, 201
-
-api.add_resource(DogCreation, '/dog')
+class DogRequests():
+    def __init__(self, **dict):
+        self.name = dict['name']
+        self.breed = dict['breed']
