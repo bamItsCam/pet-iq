9ad177e1bab139532cf2eb11456180129b935022
john <johncusackccm@gmail.com>
2018-10-10 20:01:41 -0400

    Added age to dog model

--- api/models/dog.py
+++ api/models/dog.py
@@ -1,14 +1,13 @@
 from api import db
 
-
 class Dog(db.Model):
-    __tablename__ = 'dogs'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128))
-    breed = db.Column(db.String(128))
+	__tablename__ = 'dogs'
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(128))
+	breed = db.Column(db.String(128))
     age = db.Column(db.Integer)
-    owner = db.Column(db.Integer, db.ForeignKey('owners.id'))
-    scores = db.relationship('Score', backref='dogs', lazy='dynamic')
+	owner = db.Column(db.Integer, db.ForeignKey('owners.id'))
+	scores = db.relationship('Score', backref='dogs', lazy='dynamic')
 
-    def __repr__(self):
-        return '<Dog {}/{}>'.format(self.owner.username, self.name)
+	def __repr__(self):
+		return '<Dog {}/{}>'.format(self.owner.username,self.name)
