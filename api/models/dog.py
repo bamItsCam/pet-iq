db4489e7f6902f2a0ee595a0756b8f27da72b316
john <johncusackccm@gmail.com>
2018-10-10 20:25:39 -0400

    Fixed tab spacing issue maybe, not really

--- api/models/dog.py
+++ api/models/dog.py
@@ -1,14 +1,13 @@
 from api import db
 
-
 class Dog(db.Model):
-    __tablename__ = 'dogs'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128))
-    breed = db.Column(db.String(128))
-    age = db.Column(db.Integer)
-    owner = db.Column(db.Integer, db.ForeignKey('owners.id'))
-    scores = db.relationship('Score', backref='dogs', lazy='dynamic')
+	__tablename__ = 'dogs'
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(128))
+	age = db.Column(db.Integer)
+	breed = db.Column(db.String(128))
+	owner = db.Column(db.Integer, db.ForeignKey('owners.id'))
+	scores = db.relationship('Score', backref='dogs', lazy='dynamic')
 
-    def __repr__(self):
-        return '<Dog {}/{}>'.format(self.owner.username, self.name)
+	def __repr__(self):
+		return '<Dog {}/{}>'.format(self.owner.username,self.name)
