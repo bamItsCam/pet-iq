c9cc725df5e86619925fc6296acb63099578ac18
john <johncusackccm@gmail.com>
2018-10-20 10:39:16 -0400

    added redux stuff from tut

---
+++ client/src/api/userApi.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+const API = 'http://127.0.0.1:5000/api/v1/';
+const DEFAULT_QUERY = 'owner'
+
+
+class UserApi {  
+    static getAllUsers() {
+      return axois.get(API + DEFAULT_QUERY).then(response => {
+        return response.data;
+      }).catch(error => {
+        return error;
+      });
+    }
+  
+  export default UserApi;  
